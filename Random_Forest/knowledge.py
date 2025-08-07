31b85ec061bbdefc7e7cb53a89864c36cf2ac985
lmy12367 <lfuturemy@163.com>
2025-08-07 22:28:35 +0800

    bagging

--- Random_Forest/knowledge.py
+++ Random_Forest/knowledge.py
@@ -6,3 +6,10 @@ from sklearn.ensemble import BaggingClassifier,RandomForestClassifier
 x,y=make_classification(n_samples=1000,n_features=16,n_classes=2,random_state=42)
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
 
+bagging=BaggingClassifier(
+    base_estimator=DecisionTreeClassifier(),
+    n_estimators=100,
+    random_state=42
+)
+bagging.fit(x_train,y_train)
+print("acc=",bagging.score(x_test,y_test))
