8c69598e8723bf681876c291e619daeeee8e8c5c
lmy12367 <lfuturemy@163.com>
2025-08-07 22:14:14 +0800

    data

---
+++ Random_Forest/knowledge.py
@@ -0,0 +1,8 @@
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import BaggingClassifier,RandomForestClassifier
+
+x,y=make_classification(n_samples=1000,n_features=16,n_classes=2,random_state=42)
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+
