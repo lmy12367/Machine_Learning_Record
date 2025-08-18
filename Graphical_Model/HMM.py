2ab33a9eee894ff3b83c8d4bc06a9355301a684f
lmy12367 <lfuturemy@163.com>
2025-08-18 17:52:30 +0800

    data

---
+++ Graphical_Model/HMM.py
@@ -0,0 +1,18 @@
+import matplotlib.image as mping
+import matplotlib.pyplot as plt
+import numpy as np
+
+orig_img = np.array(mping.imread('./origimg.jpg'), dtype=int)
+orig_img[orig_img < 128] = -1
+orig_img[orig_img >= 128] = 1
+
+noisy_img = np.array(mping.imread('noisyimg.jpg'), dtype=int)
+noisy_img[noisy_img < 128] = -1
+noisy_img[noisy_img >= 128] = 1
+
+def compute_noise_rate(noisy,orig):
+    err=np.sum(noisy!=orig)
+    return err/orig.size
+
+init_noise_rate=compute_noise_rate(noisy_img,orig_img)
+print (f'带噪图像与原图不一致的像素比例：{init_noise_rate * 100:.4f}%')
